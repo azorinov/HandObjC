9dd72b0535062ef916ffa60d99307ef351b0e193
azorinov <azorinov@hotmail.com>
2023-09-24 08:20:39 +0300

    Initial Commit

---
+++ HandObjC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HandObjC
+//
+//  Created by Alexander Zorinov on 2023-09-24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
